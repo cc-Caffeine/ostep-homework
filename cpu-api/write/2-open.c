fd65c58b82c42e07e1756817922a5a05dd6961b8
cc-Caffeine <summersay1@outlook.com>
2026-07-14 00:31:27 +0800

    upd 2-open

--- cpu-api/write/2-open.c
+++ cpu-api/write/2-open.c
@@ -1,3 +1,26 @@
+#include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
 
-int main() {}
+int main() {
+  char *textParent = "Hello parent\n";
+  char *textChild = "Hello child\n";
+
+  int fd = open("./text.txt", O_CREAT | O_WRONLY, 0644);
+  if (fd == -1) {
+    perror("open");
+  }
+
+  int rc = fork();
+  if (rc < 0) {
+    fprintf(stderr, "fork failed\n");
+  } else if (rc == 0) {
+    write(fd, textChild, 13);
+  } else {
+    write(fd, textParent, 13);
+  }
+
+  return 0;
+}
+
+// 都可以访问text.txt
