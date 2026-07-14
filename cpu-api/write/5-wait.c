d53f4e4d480e2055c95dfdd9d5ad0133055b9c22
cc-Caffeine <summersay1@outlook.com>
2026-07-14 11:17:21 +0800

    upd 5-wait.c

---
+++ cpu-api/write/5-wait.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <threads.h>
+#include <unistd.h>
+
+int main() {
+  int rc = fork();
+  if (rc == 0) {
+    printf("hello child\n");
+  } else {
+    int rc_wait = wait(NULL);
+    printf("hello parent\n");
+  }
+
+  return 0;
+}
